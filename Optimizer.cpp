434aa058ff739f3d7abe620d1a38195e79998440
XuhangLiu2008 <2451233667@qq.com>
2026-01-19 23:37:00 +0800

    idk, so much changed

--- Optimizer.cpp
+++ Optimizer.cpp
@@ -9,9 +9,18 @@
 
 
 
+static random_device rd;
+static mt19937 gen(rd());
+
+
+
 pair<unique_ptr<vector<int>>, unique_ptr<vector<int>>> simpleSimulatedAnnealing::_randDisturb() {
     // layer_index and target_fila
     // can be directly put input core_mat.BatchModify
+
+    vector<int> layer_index, target_fila;
+
+    
 
     int lowerlimit = 1;
     int upperlimit = layer_size-2;
@@ -20,22 +29,59 @@ pair<unique_ptr<vector<int>>, unique_ptr<vector<int>>> simpleSimulatedAnnealing:
     // mean : (upperlimit-lowerlimit) / 2
     // default std_dev : (upperlimit-lowerlimit)/6
 
-    random_device rd;
-    mt19937 gen(rd());
-    normal_distribution<> dist((upperlimit-lowerlimit) / 2, (upperlimit-lowerlimit)/6);
-
-    vector<int> layer_index, target_fila;
+    float std_dev =  (upperlimit-lowerlimit)/6;
+    if (configs.contains("std_dev"))
+        std_dev = configs["std_dev"];
+    normal_distribution<> norm_dist((upperlimit-lowerlimit) / 2, std_dev);
 
     // random layer_index
-    for (int _ = 0; _ < batch_size; _++) {
-        int pos;
+    for (int batch_index = 0; batch_index < batch_size; batch_index++) {
+        int pos = static_cast<int>(norm_dist(gen));
+        while (!((pos >= lowerlimit) &&
+                 (pos <= upperlimit) &&
+                 (
+                     (core_mat_ptr->fila_list[batch_index][pos].item<int>() != 0)   ||
+                     (core_mat_ptr->fila_list[batch_index][pos-1].item<int>() != 0) ||
+                     (core_mat_ptr->fila_list[batch_index][pos+1].item<int>() != 0)
+                 )
+                 )) {
+            pos = static_cast<int>(norm_dist(gen));
+        }
+        layer_index.push_back(pos);
+    }
+
+
+
+    uniform_real_distribution<> uni_float_dist(0, 1);
+    uniform_int_distribution<> uni_int_dist(1, fila_group->num_fila-1);
+
+    float air_ratio = configs.contains("air_ratio") ? configs["air_ratio"].get<float>() : 0.2;
+
+    // random target_fila
+    for (int batch_index = 0; batch_index < batch_size; batch_index++) {
+        int pos = layer_index[batch_index];
+
+        if (core_mat_ptr->fila_list[batch_index][pos].item<int>() == 0) { // extend
+            target_fila.push_back(uni_int_dist(gen));
+        }
+        else if (core_mat_ptr->fila_list[batch_index][pos-1].item<int>() == 0 &&
+                core_mat_ptr->fila_list[batch_index][pos+1].item<int>() == 0) {
+                target_fila.push_back(uni_int_dist(gen)); // the only one in the list
+        }
+        else {
+            target_fila.push_back(uni_float_dist(gen) < air_ratio ? 0 : uni_int_dist(gen));
+            // remove or modify
+        }
 
     }
+
+    return make_pair(make_unique<vector<int>>(layer_index), make_unique<vector<int>>(target_fila));
+
 }
 
 torch::Tensor simpleSimulatedAnnealing::solve() {  // that is freaking dam sit rubbish
 
-    batch_size = target_pic_FRONTLIGHT.numel(); // sizes = {H, W, 3}
+    batch_size = target_pic_FRONTLIGHT.numel() / 3; // sizes = {H, W, 3}
     layer_size = configs["layer_size"];
 
     BatchExpectPassMatrix core_mat = BatchExpectPassMatrix(batch_size, layer_size, fila_group);
@@ -43,6 +89,16 @@ torch::Tensor simpleSimulatedAnnealing::solve() {  // that is freaking dam sit r
 
     // Set initial values
 
+    uniform_int_distribution<> dist(1, fila_group->num_fila-1);
+    // -1, except for AIR
+
+    torch::Tensor BatchFilaList = torch::zeros({batch_size, layer_size}, torch::kUInt8);
+    for (int batch_index = 0 ; batch_index < batch_size; batch_index++) {
+        // init in the middle
+        BatchFilaList[batch_index][(layer_size - 1) / 2] = dist(gen);
+    }
+
+    core_mat_ptr->SetMatrix(&BatchFilaList);
 
     // SA
 }
