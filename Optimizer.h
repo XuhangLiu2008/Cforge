4cee2c547bf0096d783f3ea256770623aae6e17d
XuhangLiu2008 <2451233667@qq.com>
2026-01-19 10:28:22 +0800

    Optimizer as an abstract base class

--- Optimizer.h
+++ Optimizer.h
@@ -10,17 +10,60 @@
 #include "Filaments.h"
 #include "BatchExpectPassMatrix.h"
 #include "_debugUtilis.h"
+#include <map>
 
 using namespace std;
 
 class Optimizer { // abstract base class
     public:
-        Optimizer();
+
+        explicit Optimizer(FilaGroup* fila_group,
+            const map<string, torch::Tensor> &configs,
+            const torch::Tensor* target_pic_FRONTLIGHT,
+            const torch::Tensor* target_pic_BACKLIGHT,
+            const torch::Tensor* weight_FRONTLIGHT,
+            const torch::Tensor* weight_BACKLIGHT) {
+
+            this->fila_group = fila_group;
+            this->configs = configs;
+
+            this->target_pic_FRONTLIGHT = *target_pic_FRONTLIGHT;
+            this->target_pic_BACKLIGHT = *target_pic_BACKLIGHT;
+            this->weight_FRONTLIGHT = *weight_FRONTLIGHT;
+            this->weight_BACKLIGHT = *weight_BACKLIGHT;
+        };
+
+        explicit Optimizer(FilaGroup* fila_group,
+            const map<string, torch::Tensor> &configs,
+            const torch::Tensor* target_pic_FRONTLIGHT,
+            const torch::Tensor* target_pic_BACKLIGHT) {
+
+            this->fila_group = fila_group;
+            this->configs = configs;
+            this->target_pic_FRONTLIGHT = *target_pic_FRONTLIGHT;
+            this->target_pic_BACKLIGHT = *target_pic_BACKLIGHT;
+        };
+
+        FilaGroup* fila_group;
+
+        torch::Tensor target_pic_FRONTLIGHT;
+        torch::Tensor target_pic_BACKLIGHT;
+
+        torch::Tensor weight_FRONTLIGHT;
+        torch::Tensor weight_BACKLIGHT;
+
+        map<string, torch::Tensor> configs;
+        // depends
+
+        virtual torch::Tensor solve();
+        // BatchFilaLists returned
+
     private:
-        BatchExpectPassMatrix core_mat;
+
+        unique_ptr<BatchExpectPassMatrix> core_mat_ptr;
 };
 
-// variaties
+// variations
 
 
 // factory class
